ef9ac502844083c01b903997dba233a1539c3174
valentinCorrea25 <correavalentin89@gmail.com>
2024-09-15 20:38:56 -0300

    kjsadk

--- app/views/producto.php
+++ app/views/producto.php
@@ -31,7 +31,7 @@
             <div id="table">
                 <table>
                 <tr>
-                    <th> CATEGORIA</th>
+                    <th> CATEGORIA Sape</th>
                     <td><?=$prd["categoria"]?></td>
                 </tr>
                 <tr>
